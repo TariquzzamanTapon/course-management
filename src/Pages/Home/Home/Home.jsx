a5111aa9d60d455ee8d263c890be607db3f69f91
TarifHasan <tariquzzaman.tapon@gmail.com>
2023-06-16 16:46:27 +0600

    helmet are use

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title> Student | Home</title>
+            </Helmet>
             this is home
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './Routes/router.jsx';
+import { HelmetProvider } from 'react-helmet-async';
 
+const helmetContext = {};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <HelmetProvider context={helmetContext}>
+      <RouterProvider router={router}></RouterProvider>
+    </HelmetProvider>
   </React.StrictMode>,
 )
